90958dc62d9928c72f4cc8691f3ae5732226f0b5
Menno van der Sman <menno@wakoopa.com>
2010-01-20 16:48:29 +0100

    Add support for parsing urls with wildcards

--- lib/domainatrix.rb
+++ lib/domainatrix.rb
@@ -1,6 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__)) unless $LOAD_PATH.include?(File.dirname(__FILE__))
 
-require 'uri'
+require 'addressable/uri'
 require 'domainatrix/domain_parser.rb'
 require 'domainatrix/url.rb'
 
